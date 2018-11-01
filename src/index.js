64df09096f23a4a382bfcf1b16de87604c1039b4
Phillip Steele <Phillip.Steele@sage.com>
2018-11-01 10:48:25 +0000

    Update with app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const model= {
+    running : false,
+    time: 0
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let view = (m) => {
+    let mins = Math.floor(m.time / 60);
+    let secs = m.time - mins * 60;
+    let formattedSeconds = secs < 10 ? '0' + secs : secs;
+    let stopStartHandler = (event) => {
+        update(model, m.running ? 'STOP' : 'START');
+    };
+    let resetHandler = (event) => {
+        update(model, 'RESET');
+    };
+    return (
+        <div>
+            <p>{mins}:{formattedSeconds}</p>
+            <button onClick={stopStartHandler}>{m.running ? 'Stop' : 'Start'}</button>
+            <button onClick={resetHandler}>Reset</button>
+        </div>
+    );
+};
+
+let intents = {
+    TICK : 'TICK',
+    START: 'START',
+    STOP: 'STOP',
+    RESET: 'RESET'
+}
+
+const getNewTime = (model) => {
+    return model.running ? model.time + 1 : model.time;
+}
+
+const update = (model, intent) => {
+    const updates = {
+        'TICK':(model) => Object.assign(model, {time: getNewTime(model)}),
+        'START':(model) => Object.assign(model, {running:true}),
+        'STOP':(model) => Object.assign(model, {running:false}),
+        'RESET':(model) => Object.assign(model, {
+            running:false,
+            time:0
+        })
+    };
+    updates[intent](model);
+    render();
+}
+
+let render = () => ReactDOM.render(view(model), document.getElementById('root'));
+
+setInterval(() => {
+    update(model, 'TICK');
+}, 1000);
+
+render();
